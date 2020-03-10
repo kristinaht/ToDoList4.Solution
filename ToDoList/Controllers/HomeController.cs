1d3814117ef13b28592c0998c5cdde2b0a920edb
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-10 08:37:11 -0700

    create [Route('/items')] Create() route for adding new items to list

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -17,5 +17,13 @@ namespace ToDoList.Controllers
     {
       return View();
     }
+
+  [Route("/items")]
+    public ActionResult Create(string description)
+    {
+      Item myItem = new Item(description);
+      return View("Index", myItem);
+      //"Index" specifies the view that should be returned because we are no longer routing to a view with the same exact name as our route method.
+    }
   }
 }
